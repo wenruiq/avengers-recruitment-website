b4b2c76f9e35ca9406aba9dad9a6d0b76c216ffc
wenruiq <58852708+wenruiq@users.noreply.github.com>
2020-05-27 17:01:41 +0800

    basic template and styling done for landing page

---
+++ dist/js/navscroll.js
@@ -0,0 +1,8 @@
+const navbar = document.getElementById('navbar');
+window.onscroll = function () {
+  if (window.pageYOffset > 100) {
+    navbar.classList.remove('top');
+  } else {
+    navbar.classList.add('top');
+  }
+};
