be383f021230365e313d95d1a8d575c1919fd160
wenruiq <58852708+wenruiq@users.noreply.github.com>
2020-05-28 18:41:05 +0800

    added work sections with villains defeated, gifs and bios are displayed in grid layouts

---
+++ dist/js/smoothscroll.js
@@ -0,0 +1,12 @@
+$('#navbar a, .btn').on('click', function (e) {
+  if (this.hash != '') {
+    e.preventDefault();
+    const hash = this.hash;
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
