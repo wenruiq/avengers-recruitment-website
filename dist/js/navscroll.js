501d38a894c7971371cf128e5836951f6a31673c
wenruiq <58852708+wenruiq@users.noreply.github.com>
2020-09-09 13:18:59 +0800

    fixed navscroll effect coming in too late

--- dist/js/navscroll.js
+++ dist/js/navscroll.js
@@ -1,6 +1,6 @@
 const navbar = document.getElementById('navbar');
 window.onscroll = function () {
-  if (window.pageYOffset > 100) {
+  if (window.pageYOffset > 15) {
     navbar.classList.remove('top');
   } else {
     navbar.classList.add('top');
